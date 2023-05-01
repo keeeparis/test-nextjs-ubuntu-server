f89c01473d69a8d6408ac002a51c3030c54cd874
Vladimir Trotsenko <vladimir.tr@musan.kz>
2023-05-01 16:32:18 +0600

    feat: app ssr one page /list

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        hostname: '127.0.0.1'
+      },
+      {
+        hostname: 'api.seesaw.kz'
+      }
+    ],
+  }
 }
 
 module.exports = nextConfig
---
+++ src/pages/list.tsx
@@ -0,0 +1,68 @@
+import { GetServerSideProps } from 'next';
+import Image from 'next/image';
+import React from 'react';
+
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_API;
+const API = `${BASE_URL}/api`;
+
+interface ListProps {
+  data: {
+    data: {
+      id: number;
+      attributes: {
+        Name: string;
+        Url: string;
+        Image: {
+          data: {
+            id: number;
+            attributes: {
+              name: string;
+              url: string;
+              width: number;
+              height: number;
+            }
+          }
+        }
+      }
+    }[],
+    meta: {
+      pagination: {
+        page: number;
+        pageSize: number;
+        pageCount: number;
+        total: number;
+      }
+    }
+  }
+}
+
+const List = ({ data }: ListProps) => {
+  console.log(data)
+  return (
+    <div>
+      <h1>List of data</h1>
+      {data.data.map(supermarket => {
+        return (
+          <div key={supermarket.id}>
+            <h2>{supermarket.attributes.Name}</h2>
+            <p>{supermarket.attributes.Url}</p>
+            <Image src={`${BASE_URL}${supermarket.attributes.Image.data.attributes.url}`} alt={supermarket.attributes.Image.data.attributes.name} width={supermarket.attributes.Image.data.attributes.width} height={supermarket.attributes.Image.data.attributes.height} />
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
+export default List;
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const response = await fetch(`${API}/supermarkets?populate=*`);
+  const data = await response.json();
+
+  return {
+    props: {
+      data
+    }
+  }
+}
